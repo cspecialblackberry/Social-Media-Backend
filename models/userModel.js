9590e51ed1b144395be58fcb52ed797b64effc8e
Hall Family <charliesamhall@icloud.com>
2024-04-02 21:26:46 -0400

    user model

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true, trimmed: true},
+    email: {
+        type: String, 
+        required: true, 
+        unique: true,
+        validate: {
+            validator: function(email) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    friends: {type: Array},
+    thoughts: {type: Array},
+    virtuals: {
+        friendCount: {
+            get() {
+                return this.friends.length
+            }
+        }
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
