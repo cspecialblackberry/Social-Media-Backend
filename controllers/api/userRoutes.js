19358eb9184bcbdb41011c54e89ef37a2c2025d3
Hall Family <charliesamhall@icloud.com>
2024-04-04 17:16:34 -0400

    fixed bugs on add friends route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const { User } = require('../../models')
-const {ObjectId} = require('mongodb')
+const { ObjectId } = require('mongodb')
 
 //get all users
 router.get('/', async (req, res) => {
-    try{
+    try {
         const response = await User.find({}).lean()
         res.send(response)
-    }catch(err){
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
@@ -15,10 +15,10 @@ router.get('/', async (req, res) => {
 
 //get one user by id
 router.get('/:id', async (req, res) => {
-    try{
+    try {
         const response = await User.findById(req.params.id).lean()
         res.send(response)
-    }catch(err){
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
@@ -26,10 +26,10 @@ router.get('/:id', async (req, res) => {
 
 //create a new user
 router.post('/', async (req, res) => {
-    try{
+    try {
         const newUser = await User.create(req.body)
         res.send(newUser)
-    }catch(err){
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
@@ -37,12 +37,12 @@ router.post('/', async (req, res) => {
 
 //edit a user by id
 router.put('/:id', async (req, res) => {
-    try{
+    try {
         const response = await User.updateOne({
             _id: new ObjectId(req.params.id)
         }, req.body)
         res.send(response)
-    }catch(err){
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
@@ -50,12 +50,12 @@ router.put('/:id', async (req, res) => {
 
 //delete a user by id
 router.delete('/:id', async (req, res) => {
-    try{
+    try {
         const response = await User.deleteOne({
             _id: new ObjectId(req.params.id)
         })
         res.send(response)
-    }catch(err){
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
@@ -63,16 +63,38 @@ router.delete('/:id', async (req, res) => {
 
 //add users to another's friends list
 router.post('/:id/friends/:friendId', async (req, res) => {
-    try{
+    try {
         const response = await User.findById(req.params.id).lean()
-        response.friends.push(req.params.friendId)
-        res.send(response)
-    }catch(err){
+        if (response) {
+            const friendResponse = await User.findById(req.params.friendId).lean()
+            if (friendResponse) {
+                response.friends.push(friendResponse._id)
+                const update = await User.updateOne({
+                    _id: new ObjectId(req.params.id)
+                }, response)
+                res.send(update)
+            }else{
+            res.send('invalid friend id')
+            }
+        }else{
+        res.send('invalid user id')
+        }
+    } catch (err) {
         console.error(err)
         res.status(400).json(err)
     }
 })
 
+router.delete('/:id/friends/:friendId', async (req, res) => {
+    try {
+        const response = await User.findById(req.params.id).lean()
+        response.friends.splice((response.friends.indexOf(req.params.friendId)), 1)
+        res.send(response)
+    } catch (err) {
+        console.error(err)
+        res.status(400).json(err)
+    }
+})
 
 
 module.exports = router
