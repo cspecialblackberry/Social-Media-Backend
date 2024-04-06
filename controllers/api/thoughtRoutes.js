9409e313b6d68fd3dc1fa2a8e146e8fe11752d90
Hall Family <charliesamhall@icloud.com>
2024-04-06 19:44:22 -0400

    thought routes

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -27,8 +27,8 @@ router.get('/:id', async (req, res) => {
 //create a new thought
 router.post('/', async (req, res) => {
     try {
-        const newUser = await Thought.create(req.body)
-        res.send(newUser)
+        const response = await Thought.create(req.body)
+        res.send(response)
     } catch (err) {
         console.error(err)
         res.status(400).json(err)
